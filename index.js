2f0fd9ed1c696856aac10c7f5e99aee54308d490
AjayKumarB S <ajaybangre0701@gmail.com>
2025-05-20 21:58:10 +0530

    Learned About What is "Value", "Variable"  and naming conventions.

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 let js = "amazing";
-if (js === "amazing") alert("js is fun");
-44 + 30 - 9;
+// if (js === "amazing") alert("js is fun");
+
 console.log(44 + 30 - 9);
+
+// Learning About Values And variables
+// FOR VARIABLES WE  CAN'T USE RESERVE WORDS AND SHOULD NOT START WITH NUMBER
+// ONLY UPPER CASE USED FOT CONSTANT VALUES
+// USE PROPER MEANING FULL VARIABLE NAME
+
+let PI = 3.1415;
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+let firstName = "Ajay";
+
+console.log(firstName);
+
+console.log(firstName);
+
+console.log(firstName);
